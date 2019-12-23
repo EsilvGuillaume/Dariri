28dfb8fe4b5accbd970d8cbe3cbf06fb343fa9f2
Guillaume Ambroise <guillaume.ambroise@kindredgroup.com>
2019-12-23 22:10:13 +0100

    Add spotify lecture

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -2,20 +2,22 @@
     <section class="l-header" id="hp-heroBIS" style="height: auto;">
     <div class="hero-container" id="homepage-hero">
         <div>
-            <div id="hp-hero" class="hp-hero">
+            <div id="hp-hero" class="hp-hero" style="z-index:9000">
                 <header id="header" class="hero-img"
                         style="height: 632px; background-image: url('https://media.disneylandparis.com/d4th/fr-fr/images/hd14899_2022jun01_world_frozen-celecration_16-9_tcm808-194768.jpg?w=1425');">
                                         <div id="hp-overlay" class="hp-overlay"></div>
                 </header>
-                    <div class="hp-hero-content" style="margin-left:-9vh; margin-top:-30vh"><h4>Embarquez pour un voyage enchanté inédit aux côtés d’Elsa, Anna et
+                <div class="hp-hero-content" style="margin-left:-9vh; margin-top:-30vh">
+                        <h4>Embarquez pour un voyage enchanté inédit aux côtés d’Elsa, Anna et
                         leurs amis !</h4>
                         <h1>Célébration de La Reine des Neiges</h1><h4>le X DATE 2020</h4>
                         <h4>Retrouvez vite votre Zinzin à la date prévue, pour un merveilleux séjour
-                           <br/> <a style="color:white" href="http://les-saisons.disneylandparis.com/fr-fr/celebration-de-la-reine-des-neiges">
-                                Pour plus d'info clique ici :D
+                           <br/> <a style="color:black" href="http://les-saisons.disneylandparis.com/fr-fr/celebration-de-la-reine-des-neiges">
+                                Pour plus d'info clique ici :D ou descends écouter du BON SON
                             </a>
                         </h4>
                     </div>
+                <iframe src="https://open.spotify.com/embed/album/4M07HWIlZr7zoXoxDHR5mz" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             </div>
         </div>
     </div>
