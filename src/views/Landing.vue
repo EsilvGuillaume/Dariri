c66d85ae75855bfb7913c0be73c6e81f7a907924
Guillaume Ambroise <guillaume.ambroise@kindredgroup.com>
2020-01-05 18:58:56 +0100

    Add date

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -7,10 +7,10 @@
                         style="height: 632px; background-image: url('https://media.disneylandparis.com/d4th/fr-fr/images/hd14899_2022jun01_world_frozen-celecration_16-9_tcm808-194768.jpg?w=1425');">
                                         <div id="hp-overlay" class="hp-overlay"></div>
                 </header>
-                <div class="hp-hero-content" style="margin-left:-9vh; margin-top:-30vh">
+                <div class="hp-hero-content" style="margin-left:-7vh; margin-top:-40vh">
                         <h4>Embarquez pour un voyage enchanté inédit aux côtés d’Elsa, Anna et
                         leurs amis !</h4>
-                        <h1>Célébration de La Reine des Neiges</h1><h4>le X DATE 2020</h4>
+                        <h1>Célébration de La Reine des Neiges</h1><h4>le 22 et 23 janvier 2020</h4>
                         <h4>Retrouvez vite votre Zinzin à la date prévue, pour un merveilleux séjour
                            <br/> <a style="color:black" href="http://les-saisons.disneylandparis.com/fr-fr/celebration-de-la-reine-des-neiges">
                                 Pour plus d'info clique ici :D ou descends écouter du BON SON
